af3fda8bf1677d9aa78c89d4c5e2348b9e99cc82
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-28 10:13:59 -0900

    user and cart slice done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import { Button } from "./components/ui/button"
-import { create } from "zustand"
-
-const useStore = create<{
-  count: number
-  inc: () => void
-  dec: () => void
-}>((set) => ({
-  count: 0,
-  inc: () => set((state) => ({count: state.count + 1})),
-  dec: () => set((state) => ({count: state.count - 1})),
-}))
 
 function App() {
-  const { count, inc, dec } = useStore()
   return (
-    <div className="flex flex-col gap-4" >
-      <div className="text-2xl" >{count}</div>
-      <div className="flex gap-2 items-center" >
-      <Button onClick={inc} >Inc</Button>
-      <Button onClick={dec} >Dec</Button>
-
-      </div>
+    <div className="" >
+      
     </div>
   )
 }
---
+++ src/store/store.ts
@@ -0,0 +1,10 @@
+import { Store } from "@/types/store";
+import { create } from "zustand";
+import { createUserSlice } from "./user-slice";
+import { createCartSlice } from "./cart-slice";
+import { immer } from "zustand/middleware/immer";
+
+export const useStore = create<Store>()(immer((...a) => ({
+  ...createUserSlice(...a),
+  ...createCartSlice(...a)
+})));
