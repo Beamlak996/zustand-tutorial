cbd82b20ea1dc0720d325cabeaa7846c00f363fb
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-29 10:08:31 -0900

    cart component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Cart } from "./components/cart";
 import { ChangeQtyButton } from "./components/change-qty-button";
 import { Button } from "./components/ui/button";
 import {
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <main className="space-y-2 dark h-screen bg-background max-w-sm mx-auto mt-2">
+      <Cart />
       <h1 className="text-2xl">Products:</h1>
       <div className="space-y-2">
         {PRODUCTS_DATA.map((product) => (
---
+++ src/components/cart.tsx
@@ -0,0 +1,53 @@
+import { useStore } from "@/store/store"
+import { useShallow } from "zustand/react/shallow";
+import { Button } from "./ui/button"
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
+import { ShoppingCart, CircleX, Trash2 } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { ChangeQtyButton } from "./change-qty-button";
+
+
+export const Cart = () => {
+    const { reset, products, removeProduct, total } = useStore(
+      useShallow((state) => ({
+        reset: state.reset,
+        products: state.products,
+        removeProduct: state.removeProduct,
+        total: state.total
+      }))
+    );
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild >
+                <Button variant="secondary" size="icon" >
+                    <ShoppingCart />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="max-h-[600px] overflow-y-auto  space-y-2 w-96" >
+                <div className="flex gap-2 text-lg items-center" >
+                    <h1>Cart:</h1>
+                    <Button onClick={reset} variant="destructive" size="icon" >
+                        <CircleX />
+                    </Button>
+                </div>
+                <div className="space-y-2" >
+                    {products.map((product) => (
+                        <Card className="flex flex-col" key={product.id} >
+                            <CardHeader className="flex flex-row items-center gap-2" >
+                                <CardTitle>{product.title}</CardTitle>
+                                <Button onClick={() => removeProduct(product.id)} variant="destructive" size="icon" >
+                                    <Trash2 />
+                                </Button>
+                            </CardHeader>
+                            <CardContent>
+                                <ChangeQtyButton productId={product.id} />
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+                <p>Total: {total}$</p>
+            </PopoverContent>
+        </Popover>
+    )
+}
